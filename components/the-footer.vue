e9d867e1947422728a915cfda8af89067a13a048
Igor <makowskiigor@gmail.com>
2022-11-18 23:40:40 +0100

    feat(components): create footer

---
+++ components/the-footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="flex items-end p-2">
+    <v-card class="w-screen">
+      <v-card-text class="flex justify-center">
+        Rigtch {{ new Date().getFullYear() }} &copy; All right reserved.
+      </v-card-text>
+    </v-card>
+  </footer>
+</template>
