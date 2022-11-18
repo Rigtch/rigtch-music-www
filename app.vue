01fdb4351c625b32e3684135e8f3ee5fc72635f0
Igor <makowskiigor@gmail.com>
2022-11-18 23:53:30 +0100

    feat: create default layout

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-dark-800 h-screen flex justify-center items-center">
-    <Example />
+  <div>
+    <NuxtLayout name="default">
+      <example />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app class="bg-background !min-h-screen">
+      <v-layout class="flex flex-col min-h-screen">
+        <navigation-bar />
+
+        <v-main>
+          <div class="p-4">
+            <slot />
+          </div>
+        </v-main>
+
+        <the-footer class="justify-self-end" />
+      </v-layout>
+    </v-app>
+  </div>
+</template>
