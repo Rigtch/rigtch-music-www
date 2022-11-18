cd2c4f224a751aec4cf11fecdb1e31164169c8e6
Igor <makowskiigor@gmail.com>
2022-11-18 15:40:14 +0100

    chore: configure vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,12 @@
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt', 'nuxt-windicss'],
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
