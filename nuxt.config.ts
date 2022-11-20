d48ab06f59b62e28c62106cb9a2364cdce4e42a7
Igor <makowskiigor@gmail.com>
2022-11-20 03:52:03 +0100

    chore(nuxt.config): define proxy and `NUXT_API_AUTH_URL` environment variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,26 @@
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', 'nuxt-windicss'],
+  modules: ['@pinia/nuxt', 'nuxt-windicss', 'nuxt-proxy'],
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.css',
   ],
   build: {
     transpile: ['vuetify'],
+  },
+  runtimeConfig: {
+    public: {
+      apiAuthUrl: process.env.NUXT_API_AUTH_URL,
+    },
+  },
+  proxy: {
+    options: {
+      target: process.env.NUXT_API_AUTH_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/auth': '/auth',
+      },
+      pathFilter: '/api/auth/**',
+    },
   },
   vite: {
     define: {
