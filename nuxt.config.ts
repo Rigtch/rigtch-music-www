e8807ef53bf9898b816e644b1256af77b9c55b3e
Igor <makowskiigor@gmail.com>
2022-11-26 15:08:53 +0100

    feat(nuxt.config): setup proxy for `statistics` api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,24 +15,39 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiAuthUrl: process.env.NUXT_API_AUTH_URL,
+      apiStatisticsUrl: process.env.NUXT_API_STATISTICS_URL,
     },
   },
   proxy: {
-    options: {
-      target: process.env.NUXT_API_AUTH_URL,
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/auth': '/auth',
-        '^/graphql/auth': '/graphql',
+    options: [
+      {
+        target: process.env.NUXT_API_AUTH_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/auth': '/auth',
+          '^/graphql/auth': '/graphql',
+        },
+        pathFilter: ['/api/auth/**', '/graphql/auth/**'],
       },
-      pathFilter: ['/api/auth/**', '/graphql/auth/**'],
-    },
+      {
+        target: process.env.NUXT_API_STATISTICS_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/graphql/statistics': '/graphql',
+        },
+        pathFilter: ['/graphql/statistics/**'],
+      },
+    ],
   },
   'graphql-client': {
     clients: {
       auth: {
         introspectionHost: `${process.env.NUXT_API_AUTH_URL}/graphql`,
         host: `/graphql/auth`,
+      },
+      statistics: {
+        introspectionHost: `${process.env.NUXT_API_STATISTICS_URL}/graphql`,
+        host: `/graphql/statistics`,
       },
     },
   },
