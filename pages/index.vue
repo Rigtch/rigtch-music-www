a81104d93433ab59d5d71186729f668272d83f67
Igor <makowskiigor@gmail.com>
2022-11-26 15:12:25 +0100

    feat(pages/index): implement statistics

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { useAuthStore } from '~/stores'
+import { useAuthStore, useStatisticsStore } from '~/stores'
 
 const authStore = useAuthStore()
+const statisticsStore = useStatisticsStore()
 
 definePageMeta({
   middleware: 'auth',
+})
+
+onMounted(async () => {
+  await authStore.refreshAccessToken()
+
+  if (!statisticsStore.topArtists) statisticsStore.fetchTopArtists()
+  if (!statisticsStore.topTracks) statisticsStore.fetchTopTracks()
+  if (!statisticsStore.lastTracks) statisticsStore.fetchLastTracks()
 })
 
 watch(
@@ -16,20 +25,73 @@ watch(
 </script>
 
 <template>
-  <div class="flex justify-center pt-8 md:pt-16">
-    <div class="flex gap-4">
-      <item-card
-        name="Darkthrone"
-        image="https://i.discogs.com/FZfOeoRlm1RAjMWJde6Ef4kNiuVFpBa2zgtyMJU-CIA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI1MjIx/MS0xNDE1ODc3NTQz/LTkyMDEuanBlZw.jpeg"
-        href=""
-      />
+  <div class="flex flex-col pt-8 md:pt-16">
+    <section class="flex flex-col gap-8 w-full p-4">
+      <header>
+        <h2 class="text-h4">Top Artists</h2>
+      </header>
 
-      <item-card
-        name="Paragon Belial"
-        artist="Darkthrone"
-        image="http://gavetadebaguncas.com.br/wp-content/uploads/2020/10/Darkthrone-A-Blaze-in-the-Northern-Sky-1992-300x300.jpg"
-        is-wide
-      />
-    </div>
+      <main>
+        <div class="flex flex-col items-center md:flex-row gap-4 flex-wrap">
+          <item-card
+            v-for="(artist, index) in statisticsStore.topArtists"
+            :key="artist.name"
+            :name="artist.name"
+            :image="artist.image"
+            :href="artist.href"
+            :genres="artist.genres"
+            :position="index + 1"
+            :is-wide="index === 0"
+            :is-outlined="index === 0"
+          />
+        </div>
+      </main>
+    </section>
+
+    <v-divider />
+
+    <section class="flex flex-col gap-8 w-full p-4 pt-12">
+      <header>
+        <h2 class="text-h4">Top Tracks</h2>
+      </header>
+
+      <main>
+        <div class="flex flex-col items-center md:flex-row gap-4 flex-wrap">
+          <item-card
+            v-for="(track, index) in statisticsStore.topTracks"
+            :key="track.name"
+            :name="track.name"
+            :image="track.image"
+            :href="track.href"
+            :artists="track.artists"
+            :position="index + 1"
+            :is-wide="index === 0"
+            :is-outlined="index === 0"
+          />
+        </div>
+      </main>
+    </section>
+
+    <v-divider />
+
+    <section class="flex flex-col gap-8 w-full p-4 pt-12">
+      <header>
+        <h2 class="text-h4">Last Tracks</h2>
+      </header>
+
+      <main>
+        <div class="flex flex-col items-center md:flex-row gap-4 flex-wrap">
+          <item-card
+            v-for="track in statisticsStore.lastTracks"
+            :key="track.name"
+            :name="track.name"
+            :image="track.image"
+            :href="track.href"
+            :artists="track.artists"
+            :played-at="track.playedAt"
+          />
+        </div>
+      </main>
+    </section>
   </div>
 </template>
