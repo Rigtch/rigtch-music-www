2dc4dd36e16ccff5f5c43b2fb5ad4865f69ce8ed
Igor <makowskiigor@gmail.com>
2022-11-26 11:43:26 +0100

    feat(components): create item card

---
+++ components/item-card.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  href?: string
+  artist?: string
+  artistHref?: string
+  image: string
+  isWide?: boolean
+}>()
+</script>
+
+<template>
+  <v-card :class="!isWide && '!w-50'">
+    <div :class="[isWide ? 'flex-row-reverse' : 'flex-row', 'flex']">
+      <v-img
+        :src="image"
+        :class="[isWide && 'm-2 rounded-[4px]', 'flex items-end']"
+        :cover="!isWide"
+        :width="isWide ? '192' : '200'"
+      >
+        <v-card-title v-if="!isWide">
+          <a :href="href" target="_blank">{{ name }}</a>
+        </v-card-title>
+      </v-img>
+
+      <div>
+        <v-card-title v-if="isWide" class="text-h5 cursor-pointer">
+          <a :href="href" target="_blank">{{ name }}</a>
+        </v-card-title>
+
+        <v-card-subtitle v-if="artist" :class="!isWide && 'py-2'">
+          <a
+            class="cursor-pointer hover:text-white"
+            :href="artistHref"
+            target="_blank"
+          >
+            {{ artist }}
+          </a>
+        </v-card-subtitle>
+      </div>
+    </div>
+  </v-card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,19 @@ watch(
 
 <template>
   <div class="flex justify-center pt-8 md:pt-16">
-    <example />
+    <div class="flex gap-4">
+      <item-card
+        name="Darkthrone"
+        image="https://i.discogs.com/FZfOeoRlm1RAjMWJde6Ef4kNiuVFpBa2zgtyMJU-CIA/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI1MjIx/MS0xNDE1ODc3NTQz/LTkyMDEuanBlZw.jpeg"
+        href=""
+      />
+
+      <item-card
+        name="Paragon Belial"
+        artist="Darkthrone"
+        image="http://gavetadebaguncas.com.br/wp-content/uploads/2020/10/Darkthrone-A-Blaze-in-the-Northern-Sky-1992-300x300.jpg"
+        is-wide
+      />
+    </div>
   </div>
 </template>
