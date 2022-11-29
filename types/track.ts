853790abb1451b66771fd728b48da6382849b7b5
Igor <makowskiigor@gmail.com>
2022-11-29 12:46:52 +0100

    refactor(track): change `artist` type from `string` to `Artist`

--- types/track.ts
+++ types/track.ts
@@ -1,7 +1,10 @@
+import { Artist } from './artist'
+
 export interface Track {
   name: string
-  artists: string[]
+  artists: Artist[]
   image: string
+  albumName: string
   href: string
   playedAt?: string
 }
