e800e310abfed8dc01d9cef15027a480bfb00bdf
Igor <makowskiigor@gmail.com>
2022-12-17 14:41:33 +0100

    refactor(exceptions): add collection type

--- components/exceptions-view.vue
+++ components/exceptions-view.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
-import { useExceptionStore } from '~/stores'
+import { ExceptionCollection, useExceptionStore } from '~/stores'
+
+export interface ExceptionViewProperties {
+  collection?: ExceptionCollection
+}
 
 const exceptionStore = useExceptionStore()
+
+const properties = withDefaults(defineProps<ExceptionViewProperties>(), {
+  collection: ExceptionCollection.Global,
+})
+
+const exceptions = computed(() =>
+  exceptionStore.exceptions.filter(
+    exception => exception.collection === properties.collection
+  )
+)
 </script>
 
 <template>
   <div>
     <div
-      v-for="{ message, type } in exceptionStore.exceptions"
+      v-for="{ message, type } in exceptions"
       :key="message"
       class="absolute w-screen h-screen z-50 flex justify-center items-center backdrop-filter backdrop-blur-xl"
     >
