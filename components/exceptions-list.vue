94b321df19d545d709b8c3335b651731667e3e63
Igor <makowskiigor@gmail.com>
2022-12-17 14:41:04 +0100

    feat(components): create `exceptions-list`

---
+++ components/exceptions-list.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ExceptionCollection, useExceptionStore } from '~/stores'
+
+export interface ExceptionViewProperties {
+  collection?: ExceptionCollection
+}
+
+const exceptionStore = useExceptionStore()
+
+const properties = withDefaults(defineProps<ExceptionViewProperties>(), {
+  collection: ExceptionCollection.Global,
+})
+
+const exceptions = computed(() =>
+  exceptionStore.exceptions.filter(
+    exception => exception.collection === properties.collection
+  )
+)
+</script>
+
+<template>
+  <div>
+    <div
+      v-for="{ message, type } in exceptions"
+      :key="message"
+      class="flex flex-col gap-4 items-center"
+    >
+      <v-alert :type="type">
+        <div class="flex flex-col gap-2 items-start">
+          <div>{{ message }}</div>
+        </div>
+      </v-alert>
+    </div>
+  </div>
+</template>
